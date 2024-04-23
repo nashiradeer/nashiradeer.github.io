a1bad7bcb8bbf6c62d687784a04d7964e00f84c2
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 00:10:34 -0300

    feat: create a component with all fursonas

---
+++ src/lib/fursonas.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Fursona from './components/fursona.svelte';
+	import { assets } from '$app/paths';
+</script>
+
+<section>
+	<h2>Fursonas</h2>
+	<div>
+		<Fursona image="{assets}/fursonas/sirius.png" name="Sirius Liansheko" />
+		<Fursona image="{assets}/fursonas/sinabar.png" name="Sinabar" />
+		<Fursona image="{assets}/fursonas/hellion.png" name="Hellion" />
+		<Fursona image="{assets}/fursonas/kitsune.png" name="Kitsu'ne" />
+		<Fursona image="{assets}/fursonas/hirsuan_zoroark.png" name="Hirsuan Zoroark" />
+	</div>
+</section>
+
+<style>
+	section {
+		background-color: #8400ff;
+		padding: 4% 10%;
+		margin: 0;
+	}
+
+	h2 {
+		font-size: 3rem;
+		margin: 0;
+		margin-bottom: 5%;
+		font-weight: bolder;
+	}
+
+	div {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+		align-items: center;
+	}
+
+	@media (max-width: 768px) {
+		h2 {
+			text-align: center;
+		}
+	}
+</style>
