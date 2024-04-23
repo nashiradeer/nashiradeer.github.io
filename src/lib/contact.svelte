ac3bfc439c0ade53c975c479725869c0110ea929
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 00:10:50 -0300

    feat: create contact component

---
+++ src/lib/contact.svelte
@@ -0,0 +1,96 @@
+<section>
+	<h2>Contact</h2>
+	<ul>
+		<li>
+			<span class="material-symbols-rounded">mail</span>
+			<a href="mailto:nashiradeer@gmail.com">nashiradeer@gmail.com</a>
+		</li>
+		<li>
+			<span class="material-symbols-rounded">mail</span>
+			<a href="mailto:nashiradeer@outlook.com">nashiradeer@outlook.com</a>
+		</li>
+		<li>
+			<img src="https://cdn.simpleicons.org/github/white" alt="github" />
+			<a href="https://github.com/nashiradeer">github.com/nashiradeer</a>
+		</li>
+		<li>
+			<img src="https://cdn.simpleicons.org/youtube/white" alt="youtube" />
+			<a href="https://youtube.com/nashiradeer">youtube.com/nashiradeer</a>
+		</li>
+		<li>
+			<img src="https://cdn.simpleicons.org/twitter/white" alt="twitter" />
+			<a href="https://twitter.com/nashiradeer">twitter.com/nashiradeer</a>
+		</li>
+		<li>
+			<img src="https://cdn.simpleicons.org/bluesky/white" alt="bluesky" />
+			<a href="https://bsky.app/profile/nashiradeer.com">bsky.app/nashiradeer.com</a>
+		</li>
+	</ul>
+</section>
+
+<style>
+	section {
+		background-color: #0059ff;
+		padding: 2% 10%;
+		margin: 0;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h2 {
+		font-size: 3rem;
+		margin: 0;
+		font-weight: bolder;
+	}
+
+	a {
+		font-size: 1.5rem;
+		color: #fff;
+		text-wrap: nowrap;
+		text-decoration: none;
+	}
+
+	ul {
+		padding-right: 5%;
+	}
+
+	li {
+		display: flex;
+		align-items: center;
+	}
+
+	li .material-symbols-rounded {
+		margin: 0;
+		font-size: 2rem;
+		margin-right: 2%;
+	}
+
+	li img {
+		margin-left: 1%;
+		width: 1.7rem;
+		margin-right: 3%;
+	}
+
+	@media (max-width: 768px) {
+		section {
+			flex-direction: column;
+			justify-content: center;
+			padding: 4% 0;
+		}
+
+		ul {
+			padding-right: 0;
+			padding-left: 0;
+		}
+
+		li {
+			margin-bottom: 2%;
+		}
+
+		li img {
+			margin-left: 0;
+		}
+	}
+</style>
