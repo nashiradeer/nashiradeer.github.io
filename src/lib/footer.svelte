ef97625d0c6c10bfb8c7fbdb113d61763a038276
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 00:11:08 -0300

    feat: create footer component

---
+++ src/lib/footer.svelte
@@ -0,0 +1,25 @@
+<footer>
+	<p>
+		This website source is licensed with AGPL-3.0-or-later and is available in <a
+			href="https://github.com/nashiradeer/nashiradeer.github.io">GitHub</a
+		>.
+	</p>
+	<p>The assets found in this project can have restrictive permissions.</p>
+	<p>© Nashira Deer 2024. All Rights Reserved.</p>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		padding: 3% 0;
+	}
+
+	footer a {
+		color: #fff;
+	}
+
+	p {
+		font-size: 1.3rem;
+		margin: 0.8rem;
+	}
+</style>
