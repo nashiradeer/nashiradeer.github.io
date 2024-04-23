6ade19b49c7e25e47d70508408042f9877ac2a7b
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 14:10:13 -0300

    feat: create a component only for badges

---
+++ src/lib/badge.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	export let name: string | undefined = undefined;
+	export let image: string | undefined = undefined;
+
+	let badgeClass = 'badge-empty';
+	if (name && image) {
+		badgeClass = 'badge-text-and-image';
+	} else if (image) {
+		badgeClass = 'badge-image-only';
+	} else if (name) {
+		badgeClass = 'badge-text-only';
+	}
+</script>
+
+<div class={badgeClass}>
+	{#if image && name}
+		<img width="20" height="20" src={image} alt="Badge" />
+	{:else if image}
+		<img width="20" height="20" src={image} alt="Badge" />
+	{/if}
+	{#if name}
+		<span>{name}</span>
+	{/if}
+</div>
+
+<style>
+	div {
+		display: inline;
+		background-color: #000;
+		color: #fff;
+		font-size: 1rem;
+		padding: 5px 6px;
+		width: fit-content;
+		border-radius: 8px;
+	}
+
+	.badge-image-only {
+		padding: 5px 6.9px;
+	}
+
+	.badge-empty {
+		display: none;
+	}
+
+	img {
+		vertical-align: middle;
+	}
+
+	.badge-text-and-image img {
+		margin-right: 1px;
+	}
+
+	span {
+		vertical-align: middle;
+	}
+</style>
