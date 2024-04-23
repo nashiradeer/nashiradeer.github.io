1bd365a1e884d2f175ed856093b840c1ff0a8088
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 20:20:17 -0300

    feat: create a list with ready-to-use badges

---
+++ src/lib/badges.ts
@@ -0,0 +1,66 @@
+import { assets } from '$app/paths';
+
+export interface IBadge {
+	name: string;
+	image: string;
+}
+
+export const CPP: IBadge = {
+	name: 'C++',
+	image: `${assets}/langs/cpp.svg`
+};
+
+export const CSHARP: IBadge = {
+	name: 'C#',
+	image: `${assets}/langs/csharp.svg`
+};
+
+export const DART: IBadge = {
+	name: 'Dart',
+	image: `${assets}/langs/dart.svg`
+};
+
+export const DOCKER: IBadge = {
+	name: 'Docker',
+	image: `${assets}/langs/docker.svg`
+};
+
+export const FLUTTER: IBadge = {
+	name: 'Flutter',
+	image: `${assets}/langs/flutter.svg`
+};
+
+export const HTML: IBadge = {
+	name: 'HTML',
+	image: `${assets}/langs/html.svg`
+};
+
+export const JEKYLL: IBadge = {
+	name: 'Jekyll',
+	image: `${assets}/langs/jekyll.svg`
+};
+
+export const QT: IBadge = {
+	name: 'Qt',
+	image: `${assets}/langs/qt.svg`
+};
+
+export const RUST: IBadge = {
+	name: 'Rust',
+	image: `${assets}/langs/rust.svg`
+};
+
+export const SVELTE: IBadge = {
+	name: 'Svelte',
+	image: `${assets}/langs/svelte.svg`
+};
+
+export const TYPESCRIPT: IBadge = {
+	name: 'TypeScript',
+	image: `${assets}/langs/typescript.svg`
+};
+
+export const UNITY: IBadge = {
+	name: 'Unity',
+	image: `${assets}/langs/unity.svg`
+};
