3a70c566343179464f6c723fa55def5868324397
Nashira Deer <nashiradeer@gmail.com>
2024-04-24 14:21:51 -0300

    feat: implement new skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,10 @@
 	import { assets } from '$app/paths';
 	import Header from '$lib/header.svelte';
 	import Contact from '$lib/contact.svelte';
-	import Fursonas from '$lib/fursonas.svelte';
+	import Fursonas from '$lib/fursona.svelte';
 	import Footer from '$lib/footer.svelte';
 	import Showcase from '$lib/showcase.svelte';
+	import ContentBox from '$lib/content_box.svelte';
 </script>
 
 <svelte:head>
@@ -20,9 +21,24 @@
 </svelte:head>
 
 <Header />
-<Contact />
-<Fursonas />
-<Showcase />
+<ContentBox title="Contact" color="#0059ff">
+	<Contact />
+</ContentBox>
+<ContentBox title="About Me">
+	<p>
+		Hello! I'm Nashira Deer, a furry artist and web developer. I'm a Protogen, a species created by
+		Malice-Risu. I'm a non-binary person who uses they/them pronouns. I'm a hobbyist artist who
+		enjoys drawing furries and other anthropomorphic characters. I'm also a web developer who enjoys
+		creating websites and web applications. I'm currently learning SvelteKit, a framework for
+		building web applications with Svelte.
+	</p>
+</ContentBox>
+<ContentBox title="Fursonas" color="#8400ff">
+	<Fursonas />
+</ContentBox>
+<ContentBox title="Projects" color="#fb0072" big_content={true}>
+	<Showcase />
+</ContentBox>
 <Footer />
 
 <style>
