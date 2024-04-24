5f7292534e2d03c31f3061896510b57841bb63f6
Nashira Deer <nashiradeer@gmail.com>
2024-04-23 23:27:00 -0300

    feat: create new component for projects

---
+++ src/lib/showcase.svelte
@@ -0,0 +1,21 @@
+<script>
+	import ContentBox from './content_box.svelte';
+	import { PROJECTS } from './projects';
+	import ShowcaseItem from './showcase_item.svelte';
+</script>
+
+<ContentBox title="Projects" color="#fb0072">
+	<div>
+		{#each PROJECTS as project}
+			<ShowcaseItem {...project} />
+		{/each}
+	</div>
+</ContentBox>
+
+<style>
+	div {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+</style>
