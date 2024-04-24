d2ca7ecc43c1c3258684835c9028c20baf2b1fd6
Nashira Deer <nashiradeer@gmail.com>
2024-04-24 11:43:18 -0300

    feat: add seo and social media tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { assets } from '$app/paths';
 	import Header from '$lib/header.svelte';
 	import Contact from '$lib/contact.svelte';
 	import Fursonas from '$lib/fursonas.svelte';
@@ -7,8 +8,15 @@
 </script>
 
 <svelte:head>
-	<title>Nashira Deer | Home</title>
+	<title>Nashira Deer</title>
 	<meta name="description" content="Nashira Deer's personal website." />
+	<meta property="og:title" content="Nashira Deer" />
+	<meta property="og:description" content="Nashira Deer's personal website." />
+	<meta property="og:url" content="https://www.nashiradeer.com/" />
+	<meta property="og:image" content="{assets}/favicon.png" />
+	<meta property="og:image:alt" content="Sinabar, a furry of the Protogen specie." />
+	<meta name="twitter:card" content="summary" />
+	<meta name="theme-color" content="#fb0072" />
 </svelte:head>
 
 <Header />
