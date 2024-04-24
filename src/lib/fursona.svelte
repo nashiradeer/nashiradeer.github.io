9f20782c406ecc960b503bf7468f001226d82177
Nashira Deer <nashiradeer@gmail.com>
2024-04-24 14:21:16 -0300

    refactor!: make fursona works with small ContentBox

---
+++ src/lib/fursona.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { FURSONAS } from './fursonas';
+</script>
+
+<div class="fursona">
+	{#each FURSONAS as fursona}
+		<div class="fursona-item">
+			<img
+				width="310"
+				height={fursona.image?.height ?? 310}
+				src={fursona.image?.url}
+				alt={fursona.name}
+			/>
+			<span>{fursona.name}</span>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.fursona {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: end;
+		justify-content: center;
+		gap: 20px;
+	}
+
+	span {
+		font-size: 2.2rem;
+		display: block;
+		text-align: center;
+		width: 310px;
+	}
+</style>
