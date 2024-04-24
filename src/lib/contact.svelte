2f8031e68cc92bf5f0a39ce2bd697bc659109952
Nashira Deer <nashiradeer@gmail.com>
2024-04-24 14:20:50 -0300

    refactor: make contact works with small ContentBox

--- src/lib/contact.svelte
+++ src/lib/contact.svelte
@@ -1,96 +1,79 @@
-<section>
-	<h2>Contact</h2>
-	<ul>
-		<li>
+<script lang="ts">
+	import ContentBox from './content_box.svelte';
+</script>
+
+<ul>
+	<li>
+		<a href="mailto:nashiradeer@gmail.com">
 			<span class="material-symbols-rounded">mail</span>
-			<a href="mailto:nashiradeer@gmail.com">nashiradeer@gmail.com</a>
-		</li>
-		<li>
-			<span class="material-symbols-rounded">mail</span>
-			<a href="mailto:nashiradeer@outlook.com">nashiradeer@outlook.com</a>
-		</li>
-		<li>
-			<img src="https://cdn.simpleicons.org/github/white" alt="github" />
-			<a href="https://github.com/nashiradeer">github.com/nashiradeer</a>
-		</li>
-		<li>
-			<img src="https://cdn.simpleicons.org/youtube/white" alt="youtube" />
-			<a href="https://youtube.com/nashiradeer">youtube.com/nashiradeer</a>
-		</li>
-		<li>
-			<img src="https://cdn.simpleicons.org/twitter/white" alt="twitter" />
-			<a href="https://twitter.com/nashiradeer">twitter.com/nashiradeer</a>
-		</li>
-		<li>
-			<img src="https://cdn.simpleicons.org/bluesky/white" alt="bluesky" />
-			<a href="https://bsky.app/profile/nashiradeer.com">bsky.app/nashiradeer.com</a>
-		</li>
-	</ul>
-</section>
+			<span class="name">nashiradeer@gmail.com</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://github.com/nashiradeer">
+			<img src="https://cdn.simpleicons.org/github/white" width="27" height="27" alt="github" />
+			<span class="name">github.com/nashiradeer</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://youtube.com/nashiradeer">
+			<img src="https://cdn.simpleicons.org/youtube/white" width="27" height="27" alt="youtube" />
+			<span class="name">youtube.com/nashiradeer</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://twitter.com/nashiradeer">
+			<img src="https://cdn.simpleicons.org/twitter/white" width="27" height="27" alt="twitter" />
+			<span class="name">twitter.com/nashiradeer</span>
+		</a>
+	</li>
+	<li>
+		<a href="https://bsky.app/profile/nashiradeer.com">
+			<img src="https://cdn.simpleicons.org/bluesky/white" width="27" height="27" alt="bluesky" />
+			<span class="name">bsky.app/nashiradeer.com</span>
+		</a>
+	</li>
+</ul>
 
 <style>
-	section {
-		background-color: #0059ff;
-		padding: 2% 10%;
-		margin: 0;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	h2 {
-		font-size: 3rem;
-		margin: 0;
-		font-weight: bolder;
-	}
-
 	a {
-		font-size: 1.5rem;
+		font-size: 1.3rem;
 		color: #fff;
 		text-wrap: nowrap;
 		text-decoration: none;
-	}
-
-	ul {
-		padding-right: 5%;
-	}
-
-	li {
 		display: flex;
 		align-items: center;
 	}
 
-	li .material-symbols-rounded {
+	ul {
+		padding: 0;
 		margin: 0;
-		font-size: 2rem;
-		margin-right: 2%;
+		margin-right: 120px;
+		list-style: none;
+		width: 310px;
+	}
+
+	li {
+		margin-bottom: 3px;
+	}
+
+	li .material-symbols-rounded {
+		width: 27px;
+		font-size: 1.8rem;
 	}
 
 	li img {
-		margin-left: 1%;
-		width: 1.7rem;
-		margin-right: 3%;
+		display: inline;
 	}
 
-	@media (max-width: 768px) {
-		section {
-			flex-direction: column;
-			justify-content: center;
-			padding: 4% 0;
-		}
+	li .name {
+		margin-left: 10px;
+	}
 
+	@media (max-width: 1100px) {
 		ul {
-			padding-right: 0;
-			padding-left: 0;
-		}
-
-		li {
-			margin-bottom: 2%;
-		}
-
-		li img {
-			margin-left: 0;
+			margin-right: 0;
+			margin: 0 auto;
 		}
 	}
 </style>
