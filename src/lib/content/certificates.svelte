b8e5edefc560a199e10af805183e84b68fb41360
Nashira Deer <nashiradeer@gmail.com>
2024-05-08 20:10:50 -0300

    feat: create Certificates content component

---
+++ src/lib/content/certificates.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Item from "$lib/components/item.svelte";
+	import { CERTIFICATES } from "$lib/models/certificates";
+
+
+</script>
+
+<div>
+	{#each CERTIFICATES as certificate}
+		<Item link_text="View Certificate" invertColors={true} oneLineDescription={true} {...certificate} />
+	{/each}
+</div>
+
+<style>
+	div {
+        padding-right: 50px;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+        align-items: center;
+		gap: 20px;
+		height: 80vh;
+	}
+
+    @media (max-width: 1150px) {
+        div {
+			height: auto;
+        }
+    }
+
+    @media (max-width: 1100px) {
+		div {
+            padding-right: 0;
+		}
+    }
+</style>
