b55df21c0a9b8db4f3d24ccbc08c5bef9f0b853d
Nashira Deer <nashiradeer@gmail.com>
2024-05-08 20:09:58 -0300

    feat: create the certificate models

---
+++ src/lib/models/certificates.ts
@@ -0,0 +1,26 @@
+import type { IItem } from "$lib/types";
+import { assets } from "$app/paths";
+import { CSHARP, PYTHON, QT, UNITY } from "./badges";
+
+export let CERTIFICATES: IItem[] = [
+    {
+        id: "system-programmer",
+        title: "System Programmer",
+        description: "06/2022-09/2022 | 200 hours",
+        link: `${assets}/certificates/system-programmer.pdf`,
+        image: `${assets}/certificates/senac.svg`,
+        badges: [
+            QT, PYTHON
+        ]
+    },
+    {
+        id: "game-developer",
+        title: "Game Developer",
+        description: "07/2021-10/2021 | 240 hours",
+        link: `${assets}/certificates/game-developer.pdf`,
+        image: `${assets}/certificates/senac.svg`,
+        badges: [
+            UNITY, CSHARP
+        ]
+    }
+];
