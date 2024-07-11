cb8f2c3f50a90c4a33a30ff2c3f098832b9f9c7c
Nashira Deer <nashiradeer@gmail.com>
2024-07-11 23:39:45 +0000

    feat: add home base elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>Nashira Deer // Kitsu'ne</title>
+	<title>Nashira Deer - Home</title>
 	<meta
 		name="description"
 		content="Nashira Deer's personal website containing contact information, about me, fursonas, certificates, and projects."
@@ -14,6 +14,19 @@
 	<meta property="og:image:alt" content="Sinabar, a furry of the Protogen specie." />
 	<meta name="theme-color" content="#fb0072" />
 </svelte:head>
+
+<header>
+	<img src="protogen.svg" alt="Sinabar, a furry of the Protogen specie." />
+	<h1>Nashira Deer</h1>
+	<nav>
+		<a href="/about">About</a>
+		<a href="/blog">Blog</a>
+		<a href="/fursonas">Fursonas</a>
+		<a href="/certificates">Certificates</a>
+		<a href="/projects">Projects</a>
+		<a href="/contact">Contact</a>
+	</nav>
+</header>
 
 <style>
 	:global(body) {
