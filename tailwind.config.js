ebe2e9a5e2f61aa8d5e83459556e50263aa8f83f
Nashira Deer <nashiradeer@gmail.com>
2024-08-02 01:37:34 +0000

    build(tailwind): configure TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['"Secular One"', ...defaultTheme.fontFamily.sans]
+			}
+		},
+		colors: {
+			sirius: '#0059ff',
+			sinabar: '#fb0072',
+			purrple: '#8400ff',
+			...colors
+		}
 	},
+	darkMode: [
+		'variant',
+		['@media (prefers-color-scheme: dark) { &:not(.light *) }', '&:is(.dark *)']
+	],
 	plugins: []
 };
