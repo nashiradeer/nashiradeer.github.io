5b3fab5f7688630f40bddce00f128ae19c94f905
Nashira Deer <nashiradeer@gmail.com>
2024-08-09 23:34:29 +0000

    build(vite): enable polling to support WSL

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		watch: {
+			usePolling: true
+		}
+	},
 	plugins: [sveltekit()]
 });
