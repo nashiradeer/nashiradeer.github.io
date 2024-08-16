46f2ef339e1dde529ebe00e45ccddf35e134a5b9
Nashira Deer <nashiradeer@gmail.com>
2024-08-16 01:30:00 +0000

    feat: create translations for the home page

---
+++ src/lib/i18n/langs/en.ts
@@ -0,0 +1,22 @@
+import type { Language } from '../../types';
+
+const lang: Language = {
+	Home: {
+		HeadTitle: 'Nashira Deer - Home',
+		HeadDescription:
+			"Nashira Deer's personal website containing contact information, about me, fursonas, certificates, and projects.",
+		OgTitle: 'Nashira Deer - Home',
+		OgDescription:
+			'My personal website containing contact information, about me, fursonas, certificates, and projects.',
+		OgImageAlt: 'Sinabar, a furry of the Protogen specie.',
+		LogoAlt: 'Sinabar, a furry of the Protogen specie.',
+		About: 'About',
+		Blog: 'Blog',
+		Fursonas: 'Fursonas',
+		Certificates: 'Certificates',
+		Projects: 'Projects',
+		Contact: 'Contact'
+	}
+};
+
+export default lang;
---
+++ src/lib/i18n/langs/pt.ts
@@ -0,0 +1,22 @@
+import type { Language } from '../../types';
+
+const lang: Language = {
+	Home: {
+		HeadTitle: 'Nashira Deer - Início',
+		HeadDescription:
+			'Site pessoal de Nashira Deer contendo informações de contato, sobre mim, fursonas, certificados e projetos.',
+		OgTitle: 'Nashira Deer - Início',
+		OgDescription:
+			'Meu site pessoal contendo informações de contato, sobre mim, fursonas, certificados e projetos.',
+		OgImageAlt: 'Sinabar, um furry da espécie Protogen.',
+		LogoAlt: 'Sinabar, um furry da espécie Protogen.',
+		About: 'Sobre',
+		Blog: 'Blog',
+		Fursonas: 'Fursonas',
+		Certificates: 'Certificados',
+		Projects: 'Projetos',
+		Contact: 'Contato'
+	}
+};
+
+export default lang;
