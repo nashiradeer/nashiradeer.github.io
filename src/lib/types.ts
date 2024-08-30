a975915e6c4bcb49711b3bd3d60af06ec90283b6
Nashira Deer <nashiradeer@gmail.com>
2024-08-30 00:58:03 +0000

    feat: cleanup old things

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,30 +6,3 @@ export interface PageLoad {
 export type Languages = Record<string, string | undefined>;
 
 export type Language = Record<string, Record<string, string>>;
-
-export interface IBadge {
-	name: string;
-	image: string;
-}
-
-export interface IItem {
-	id: string;
-	title: string;
-	image?: string;
-	link?: string;
-	link_text?: string;
-	description?: string;
-	enableBorder?: boolean;
-	smallTitle?: boolean;
-	badges: IBadge[];
-}
-
-export interface IFursonaImage {
-	url: string;
-	height?: number;
-}
-
-export interface IFursona {
-	name: string;
-	image?: IFursonaImage;
-}
