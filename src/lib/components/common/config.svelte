bf9410329969af3e00777da6778b3c7e165c898a
Nashira Deer <nashiradeer@gmail.com>
2024-09-07 00:34:23 +0000

    feat: create a common element for the config bar

---
+++ src/lib/components/common/config.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageLoad } from '$lib/types';
+	import LanguageSwitch from './languageSwitch.svelte';
+	import { onMount } from 'svelte';
+	import ThemeSwitch from './themeSwitch.svelte';
+
+	export let data: PageLoad;
+
+	onMount(() => {
+		new ThemeSwitch({ target: document.getElementById('config')! });
+	});
+</script>
+
+<div class="h-12 flex justify-end p-2" id="config">
+	<LanguageSwitch {data} />
+</div>
