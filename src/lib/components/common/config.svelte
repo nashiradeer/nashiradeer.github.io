92048a31f7d079ea147c533364e60d909fc216f5
NashiraDeer <nashiradeer@gmail.com>
2024-12-25 23:10:27 +0000

    feat: theme switch doesnt work when JavaScript is disabled

--- src/lib/components/common/config.svelte
+++ src/lib/components/common/config.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { mount, onMount } from 'svelte';
 	import LanguageSwitch from './languageSwitch.svelte';
 	import ThemeSwitch from './themeSwitch.svelte';
+
+	onMount(() => {
+		mount(ThemeSwitch, {
+			target: document.getElementById('config')!
+		});
+	});
 </script>
 
 <div class="h-12 flex justify-end p-2" id="config">
 	<LanguageSwitch />
-	<ThemeSwitch />
 </div>
