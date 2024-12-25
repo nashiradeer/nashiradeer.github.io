28a7f6e9e182f8c6ee8aca84fb03a04faf9abc91
NashiraDeer <nashiradeer@gmail.com>
2024-12-25 20:36:30 +0000

    docs: document what the server hook does

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
 export function handle({ event, resolve }) {
 	return resolve(event, {
+		// Replace %lang% with the language code in the HTML
 		transformPageChunk: ({ html }) => html.replace('%lang%', event.params.lang ?? 'en')
 	});
 }
