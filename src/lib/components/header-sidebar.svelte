13ec524b8bcf248e2ce44e9334fc8521ec7f43a8
NashiraDeer <nashiradeer@gmail.com>
2024-12-26 23:35:09 +0000

    feat: apply invert filter to set menu icon black on light theme

--- src/lib/components/header-sidebar.svelte
+++ src/lib/components/header-sidebar.svelte
@@ -8,6 +8,9 @@
 	let props: HeaderSidebarProps = $props();
 </script>
 
-<button onclick={props.onclick} class="sm:mr-1 mr-0.5 px-1 flex flex-row items-center sm:hidden">
+<button
+	onclick={props.onclick}
+	class="sm:mr-1 mr-0.5 px-1 flex flex-row items-center sm:hidden dark:invert-0 invert"
+>
 	<img src="/icons/menu.svg" alt={t('header', 'menu')} class="h-8 w-8" />
 </button>
